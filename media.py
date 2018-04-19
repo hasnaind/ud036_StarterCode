44d6ca91df2c4f9063b1e361d91d6c547f3c0598
Hasnan <hasnan.yaqoob@arbisoft.pk>
2018-04-19 05:52:27 -0700

    Update media.py

--- media.py
+++ media.py
@@ -8,6 +8,7 @@ class Movie(object):
         self.poster_image_url=poster_image
         self.imbd_ratings=imbd_ratings
         self.trailer_youtube_url=trailer_url
+        
     def show_trailer(self):
         #This function open a youtube page for the movie trailer
         webbrowser.open(self.trailer_youtube_url)
